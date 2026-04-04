e7d771d9635c55ccfa3fcfd168b31484a1f1ff7d
Sese Mueller <sese4dasbinich@gmail.com>
2026-04-04 02:25:10 +0200

    Added some helper lemmas and theorems about new ways to describe unboundedness (runaway universes and checkerboard universes)

--- Collatz Multiverse/Basic.lean
+++ Collatz Multiverse/Basic.lean
@@ -5,8 +5,10 @@ import Mathlib.Tactic.Ring.Basic
 import Mathlib.Tactic.Use
 
 import Mathlib.Data.Set.Finite.Basic
+import Mathlib.Data.Nat.Prime.Basic
 import Mathlib.Order.Interval.Finset.Defs
 import Mathlib.Order.Interval.Finset.Nat
+import Mathlib.Order.Preorder.Finite
 import Mathlib.Tactic.Convert
 import Mathlib.Tactic.Linarith
 import Mathlib.Tactic.Ring
@@ -51,7 +53,6 @@ def applyCollatzKTimes (u : CollatzUniverse) (n : ℕ) (k : ℕ) : ℕ :=
   else
     applyCollatzKTimes u (applyCollatz u n) (k - 1)
 
-
 -- We call a Collatz Universe a "Hub-and-Spoke" universe if the multiplier is 0.
 -- These Universes are characterized by the fact that all odd numbers are sent to the same number
 -- (the adder, and, visually, the "hub").
@@ -59,14 +60,43 @@ def isHubAndSpoke (u : CollatzUniverse) : Prop :=
   u.multiplier = 0
 
 -- Theorem: In a Hub-and-Spoke universe, all odd numbers are sent to the adder of the universe.
-theorem oddNumbersSentToSameNumber (u : CollatzUniverse) (n : ℕ) (h : isHubAndSpoke u)
+theorem oddNumbersSentToSameNumberInHubAndSpoke (u : CollatzUniverse) (n : ℕ) (h : isHubAndSpoke u)
 (hn : Odd n) : applyCollatz u n = u.adder := by
   unfold applyCollatz
   unfold isHubAndSpoke at h
   simp [h, hn]
 
--- Maybe I can also prove that in a Hub-and-Spoke universe, all numbers eventually end up
--- at the adder, but I will save that for later.
+-- In a Hub-and-Spoke universe, all numbers eventually end up
+-- at the adder. This is because even numbers keep getting divided by 2 until they become odd,
+-- and then they get sent to the adder. (Exception: 0.)
+theorem allNumbersEndUpAtAdderInHubAndSpoke (u : CollatzUniverse) (n : ℕ) (h : isHubAndSpoke u) :
+∃ k : ℕ, 0 < k → n = 0 ∨ applyCollatzKTimes u n k = u.adder := by
+  -- Proof goes here
+  -- the behaviour depends on the starting number.
+  -- I think I can do some generalized induction on the number...
+  induction n using Nat.strong_induction_on with
+  | h n ih =>
+    -- First, do the 0 case, because else it makes the whole proof more cumbersome.
+    by_cases h_zero : n = 0
+    · use 0
+      simp [h_zero]
+    -- Remove the 0 case from the rest of the goal.
+    convert_to ∃ k, 0 < k → applyCollatzKTimes u n k = u.adder
+    · simp_all only [false_or]
+    -- Depending on whether n is even or odd, we have two cases.
+    by_cases h_odd : Odd n
+    · use 1
+      unfold applyCollatzKTimes
+      simp only [zero_lt_one, one_ne_zero, ↓reduceIte, tsub_self, forall_const]
+      unfold applyCollatzKTimes
+      simp only [↓reduceIte]
+      exact oddNumbersSentToSameNumberInHubAndSpoke u n h h_odd
+    · have h_even : Even n := Nat.not_odd_iff_even.mp h_odd
+      have h_half : n / 2 < n := Nat.bitwise_rec_lemma h_zero
+      obtain ⟨k, hk⟩ : ∃ k, 0 < k → n / 2 = 0 ∨ applyCollatzKTimes u (n / 2) k = u.adder :=
+        ih (n / 2) h_half
+      sorry
+
 
 -- TODO: maybe prove the bi-angle or tri-angle nature of hub-and-spoke universes
 -- with an adder which is a power of 2?
@@ -78,6 +108,13 @@ theorem oddNumbersSentToSameNumber (u : CollatzUniverse) (n : ℕ) (h : isHubAnd
 -- a power of 2 give rise to shapes with log_2(n) sides (or more precisely, edges).
 def isShapeUniverse (u : CollatzUniverse) : Prop :=
   u.adder = 0
+
+-- In the Shape universe with multiplier 1, odd numbers are sent to themselves.
+theorem oddNumbersSentToThemselvesInShapeUniverseWithMultiplierOne (u : CollatzUniverse) (n : ℕ)
+(h : isShapeUniverse u) (h_mult : u.multiplier = 1) (h_odd : Odd n) : applyCollatz u n = n := by
+  unfold applyCollatz
+  simp_all only [← Nat.not_even_iff_odd, ↓reduceIte, one_mul, Nat.add_eq_left]
+  exact h
 
 -- We call a universe "bounded", if for all nubers, there exists a k and N such that
 -- applying the Collatz function k times to the number results in a number
@@ -331,22 +368,12 @@ applyCollatzKTimes u n k = 0 := by
     simp_all only [not_exists, not_or, ↓reduceIte];
   -- Since `applyCollatzKTimes u n k` is always even and positive, it must strictly decrease.
   have h_strict_decr : StrictAnti (fun k => applyCollatzKTimes u n k) := by
-    exact strictAnti_nat_of_succ_lt (by grind);
+    exact strictAnti_nat_of_succ_lt (by grind only [= Nat.not_odd_iff_even, = Nat.odd_iff]);
   exact absurd ( Set.infinite_range_of_injective h_strict_decr.injective )
     ( Set.not_infinite.mpr <| Set.finite_iff_bddAbove.mpr
       ⟨ _, Set.forall_mem_range.mpr fun k => h_strict_decr.antitone k.zero_le ⟩ )
 
 end AristotleLemmas
-
-
--- It is enough to show that there is some k for which all n do not grow.
-theorem isBoundedIfPathGoesLower: (∀ n : ℕ, ∃ k : ℕ, applyCollatzKTimes u n k ≤ n)
-  → isBounded u := by
-  simp only [isBounded]
-  intro h n
-  specialize h n
-  obtain ⟨k, hk⟩ := h
-  sorry
 
 -- We can show that the Shape Universes with a multiplier which is a power of 2 are bounded.
 
@@ -408,6 +435,100 @@ def isUnbounded (u : CollatzUniverse) : Prop :=
 theorem boundedEqNotUnbounded (u : CollatzUniverse) : isBounded u ↔ ¬ isUnbounded u := by
   simp [isBounded, isUnbounded]
 
+-- Alternate way to write the definition of unboundedness:
+-- There exists a number such that the function that maps k to
+-- applying the Collatz function k times to the number is not bounded above.
+def isUnbounded' (u : CollatzUniverse) : Prop :=
+  ∃ n : ℕ, (Set.range (fun k => applyCollatzKTimes u n k)).Infinite
+
+
+theorem unboundedIffUnbounded' (u : CollatzUniverse) : isUnbounded u ↔ isUnbounded' u := by
+  unfold isUnbounded isUnbounded'
+  constructor
+  · rintro ⟨n, h⟩
+    use n
+    by_contra h_finite
+    push_neg at h_finite
+    let f := fun k => applyCollatzKTimes u n k
+    let f_range := Set.range f
+    -- This set has at least one element, since it contains applyCollatzKTimes u n 0 = n.
+    have h_nonempty : f_range.Nonempty := by
+      exact Set.range_nonempty f
+    -- If the range is finite, then it has a maximum element M.
+    obtain ⟨M, hM⟩ := Set.Finite.exists_maximal h_finite h_nonempty
+    -- Now we can throw that M into h to get some k such that applyCollatzKTimes u n k > M,
+    -- which is a contradiction.
+    specialize h M
+    obtain ⟨k, hk⟩ := h
+    -- applyCollatzKTimes u n k is in the range of the function,
+    -- so it must be less than or equal to M.
+    have h_in_range : applyCollatzKTimes u n k ∈ f_range := by
+      simp only [Set.mem_range, f_range]
+      use k
+    have h_le_M : applyCollatzKTimes u n k ≤ M := by
+      exact Maximal.le hM h_in_range
+    absurd hk
+    push_neg
+    exact h_le_M
+  · rintro ⟨n, h⟩
+    use n
+    intro N
+    by_contra h_not_gt
+    push_neg at h_not_gt
+    -- From h_not_gt, it would follow that the range of the function is bounded above by N,
+    -- which contradicts h.
+    have h_finite: Set.Finite (Set.range (fun k => applyCollatzKTimes u n k)) := by
+      apply Set.finite_iff_bddAbove.mpr
+      use N
+      unfold upperBounds
+      intro x hx
+      grind only [= Set.mem_range]
+    trivial
+
+-- There exist universes which are runaway, in the sense that there exists a number
+-- such that applying the Collatz repeatedly to the number results in pure growth.
+-- This can easily be written as: the function that maps k to applying the Collatz function k times
+-- to the number is monotonically increasing.
+def isRunaway (u : CollatzUniverse) : Prop :=
+  ∃ n : ℕ, StrictMono (fun k => applyCollatzKTimes u n k)
+
+theorem runawayIffUnbounded (u : CollatzUniverse) :
+  isRunaway u → isUnbounded u := by
+  rintro ⟨n, h⟩
+  use n
+  intro N
+  by_contra h_not_gt
+  push_neg at h_not_gt
+  -- Since the function is strictly monotone, it is injective, so its range is infinite.
+  have h_infinite : Set.Infinite (Set.range (fun k => applyCollatzKTimes u n k)) := by
+    exact Set.infinite_range_of_injective h.injective
+  -- But if the function is not greater than N for any k, then its range is bounded above by N,
+  -- which contradicts the fact that the range is infinite.
+  have h_finite : Set.Finite (Set.range (fun k => applyCollatzKTimes u n k)) := by
+    apply Set.finite_iff_bddAbove.mpr
+    use N
+    unfold upperBounds
+    intro x hx
+    grind only [= Set.mem_range]
+  trivial
+
+-- Another way to write the definition of runaway universes is that there exists a number
+-- such that applying the Collatz function k+1 times to the number
+-- is greater than applying it k times, for all k.
+def isRunaway' (u : CollatzUniverse) : Prop :=
+  ∃ n : ℕ, ∀ k : ℕ, applyCollatzKTimes u n (k + 1) > applyCollatzKTimes u n k
+
+theorem runawayIffRunaway' (u : CollatzUniverse) :
+  isRunaway u ↔ isRunaway' u := by
+  constructor
+  · rintro ⟨n, h⟩
+    use n
+    intro k
+    tauto
+  · rintro ⟨n, h⟩
+    use n
+    exact strictMono_nat_of_lt_succ h
+
 -- If we are in a Shape Universe with an odd number multiplier > 2, then applying the Collatz
 -- function k times to an odd number multiplies it by multiplier ^ k.
 theorem shapeUniversesWithOddLargeTwoGrowExponentially (u : CollatzUniverse)
@@ -430,7 +551,6 @@ theorem shapeUniversesWithOddLargeTwoGrowExponentially (u : CollatzUniverse)
     rw [HnAppl]
     simp [applyCollatz, hinegeven, h]
     ring1
-
 
 
 -- We can show that the Shape Universes with odd numbers > 2 are all unbounded, as 1 explodes.
@@ -478,3 +598,185 @@ theorem shapeUniversesWithOddLargerTwoAreUnbounded (u : CollatzUniverse) (h : is
         · exact test
       · exact Nat.one_add_le_iff.mp test
   exact Nat.lt_of_succ_le (this low u.multiplier hgt2)
+
+-- In shape universes with a multiplier which is a power of 2 (that is, 2 ^ m),
+-- all numbers eventually end up in a cycle,
+-- where the cycle length is m + 1
+-- For simplicity, we will only focus on prime numbers larger than 2.
+-- Those immeadeately fall into the cycle without doing anything else.
+theorem shapeUniversesWithPowerOfTwoMultipliersAreBounded' (u : CollatzUniverse)
+(h : isShapeUniverse u) (n k : ℕ) (h_nprime : Nat.Prime n) (hk : u.multiplier = 2 ^ k)
+(h_nlg2 : 2 < n) : applyCollatzKTimes u n (k + 1) = n := by
+  unfold applyCollatzKTimes
+  simp only [Nat.add_eq_zero_iff, one_ne_zero, and_false, ↓reduceIte, add_tsub_cancel_right]
+  unfold applyCollatz
+  have h_odd : Odd n := by
+    apply Nat.Prime.odd_of_ne_two
+    · exact h_nprime
+    · exact Ne.symm (Nat.ne_of_lt h_nlg2)
+  have h_neg_even := Nat.not_even_iff_odd.mpr h_odd
+  simp_all only [reduceIte, isShapeUniverse, add_zero]
+  exact applyCollatzKTimes_power_of_two_mul u n k
+
+
+-- In shape universes with a multiplier which is even, but not a power of two,
+-- where the multiplier is of the form k1 * 2 ^ k2, with k1 odd and greater than 1,
+-- and k2 greater than 1, doing k2 steps multiplies the number by k1.
+-- This theorem is for the odd case of n, we'll do the even case in the next theorem
+theorem shapeUniversesWithEvenNonPowerOfTwoMultipliersMultiplyByOddPart (u : CollatzUniverse)
+(h : isShapeUniverse u) (k1 k2 : ℕ) (hk : u.multiplier = k1 * 2 ^ k2)
+(hk2_gt1 : k2 > 1) (n : ℕ) (h_odd : Odd n) :
+applyCollatzKTimes u n (k2 + 1) = k1 * n := by
+  unfold applyCollatzKTimes
+  have : k2 ≠ 0 := Nat.ne_zero_of_lt hk2_gt1
+  simp_all only [gt_iff_lt, ne_eq, ↓reduceIte, applyCollatz, ← Nat.not_even_iff_odd,
+  isShapeUniverse, add_zero]
+  have test := applyCollatzKTimes_pow_two_descent u k2  (n * k1) k2 (by rfl)
+  grind only
+
+-- Same as above, but for even this time.
+theorem shapeUniversesWithEvenNonPowerOfTwoMultipliersMultiplyByOddPartEven (u : CollatzUniverse)
+(h : isShapeUniverse u) (k1 k2 : ℕ) (hk : u.multiplier = k1 * 2 ^ k2)
+(hk2_gt1 : k2 > 1) (n : ℕ) (h_even : Even n) :
+applyCollatzKTimes u n (k2 + 1) = k1 * n := by
+  unfold applyCollatzKTimes
+  have : k2 ≠ 0 := Nat.ne_zero_of_lt hk2_gt1
+  simp only [Nat.add_eq_zero_iff, one_ne_zero, and_false, ↓reduceIte, add_tsub_cancel_right]
+  -- This one is significantly more difficult,
+  -- as the odd number could be at any point in the trajectory
+  sorry
+
+-- All shape universes with multipliers which are even, but not a power of two,
+-- explode nontrivially. At each multiply, it first divides as many times as the multiplier
+-- is divisible by 2, and then multiplies by the odd part of the multiplier,
+-- which is greater than 1.
+theorem shapeUniversesWithEvenNonPowerOfTwoMultipliersAreUnbounded (u : CollatzUniverse)
+(h : isShapeUniverse u) (k1 k2 : ℕ) (hk : u.multiplier = k1 * 2 ^ k2) (hk1_odd : Odd k1)
+(hk1_gt1 : k1 > 1) (hk2_gt1 : k2 > 1) : isUnbounded u := by
+  have : ¬ isBounded u ↔ isUnbounded u := by simp [boundedEqNotUnbounded]
+  rw [← this]
+  simp only [isBounded, not_forall, not_exists, not_le]
+  use 1
+  intro low
+  use low + k2
+  unfold applyCollatzKTimes applyCollatz
+  have : k2 ≠ 0 := Nat.ne_zero_of_lt hk2_gt1
+  simp_all only [isShapeUniverse, gt_iff_lt, ne_eq, Nat.add_eq_zero_iff, and_false,
+    ↓reduceIte, Nat.not_even_one, mul_one, add_zero]
+  have test := shapeUniversesWithEvenNonPowerOfTwoMultipliersMultiplyByOddPartEven
+    u h k1 k2 hk hk2_gt1 (k1 * 2 ^ k2) (by grind)
+  -- The test case operates on k2 + 1 iterations, but we are doing low + k2  -1 iterations.
+  -- We split into two calls, one of which we can replace.
+  have convert := applyCollatzKTimes_add u (k1 * 2 ^ k2) (k2 + 1) (low - 2)
+  -- In order to simplify further, we need to consider if low is less than two.
+  by_cases h : low < 2
+  · -- We can simply show that the entire right side is at least 2.
+    have right_ge_2 : 2 ≤ applyCollatzKTimes u (k1 * 2 ^ k2) (low + k2 - 1) := by
+      sorry
+    sorry
+  sorry
+
+
+
+-- End of shape universe theorems.
+
+-- Helper theorem: If a collatz universe maps an odd number to an odd number,
+-- then all odd numbers are mapped to odd numbers.
+theorem oddToOddImpliesAllOddToOdd (u : CollatzUniverse) (h : ∃ n, Odd n ∧ Odd (applyCollatz u n)) :
+∀ n, Odd n → Odd (applyCollatz u n) := by
+  obtain ⟨n₀, h_n₀_odd, h_apply_n₀_odd⟩ := h
+  intro n h_n_odd
+  unfold applyCollatz at *;
+  simp_all [← Nat.not_odd_iff_even]
+  grind only [= Nat.not_odd_iff_even, = Nat.even_add, = Nat.even_mul]
+
+-- When looking at the multiverse, it is visible that a checkerboard pattern emerges,
+-- Where, if the parity of the multiplier and adder is different,
+-- Odd numbers are sent to odd numbers.
+def isCheckerboard (u : CollatzUniverse) : Prop :=
+  (Even u.multiplier ∧ Odd u.adder) ∨ (Odd u.multiplier ∧ Even u.adder)
+
+-- On a checkerboard universe, the sum of the multiplier and adder is odd.
+lemma checkerboardImpliesSumOdd (u : CollatzUniverse) (h : isCheckerboard u) :
+  Odd (u.multiplier + u.adder) := by
+  grind only [isCheckerboard, = Nat.not_odd_iff_even, = Nat.odd_iff, = Nat.even_add, = Nat.even_iff]
+
+-- On a checkerboard universe, 1 is sent to an odd number,
+-- so all odd numbers are sent to odd numbers.
+lemma checkerboardImpliesOneToOdd (u : CollatzUniverse) (h : isCheckerboard u) :
+  Odd (applyCollatz u 1) := by
+  unfold isCheckerboard at h
+  unfold applyCollatz
+  simp only [Nat.not_even_one, ↓reduceIte, mul_one]
+  grind only [= Nat.even_iff, = Nat.not_odd_iff_even, = Nat.odd_iff, = Nat.even_add]
+
+theorem checkerboardImpliesAllOddToOdd (u : CollatzUniverse) (h : isCheckerboard u) :
+  ∀ n, Odd n → Odd (applyCollatz u n) := by
+  have h_one_to_odd : Odd (applyCollatz u 1) := checkerboardImpliesOneToOdd u h
+  apply oddToOddImpliesAllOddToOdd u
+  use 1
+  exact ⟨ by decide, h_one_to_odd ⟩
+
+-- Even stronger, in a checkerboard universe, for all k, applying the Collatz function k times to an
+-- odd number results in an odd number.
+lemma checkerboardImpliesAllOddToOddKTimes (u : CollatzUniverse) (h : isCheckerboard u) :
+  ∀ n k, Odd n → Odd (applyCollatzKTimes u n k) := by
+  have h_all_odd_to_odd : ∀ n, Odd n → Odd (applyCollatz u n) := checkerboardImpliesAllOddToOdd u h
+  intro n k h_n_odd
+  induction k with
+  | zero => simp only [applyCollatzKTimes_zero]; exact h_n_odd
+  | succ m Hm =>
+    rw [applyCollatzKTimes_add]
+    rw [applyCollatzKTimes]
+    simp [applyCollatzKTimes_zero]
+    simp_all only
+
+-- Helper theorem: With only a few exceptions, applying the Collatz function to an odd number
+-- results in a larger number. (Expections: mult = 0 or (mult = 1 and adder = 0))
+theorem oddStepGreater (u : CollatzUniverse)
+  (h_n_exception : (u.multiplier ≠ 0) ∧ (u.multiplier ≠ 1 ∨ u.adder ≠ 0)) :
+  ∀ n, Odd n → applyCollatz u n > n := by
+  intro n h_n_odd
+  unfold applyCollatz at *;
+  simp only [Nat.not_even_iff_odd.mpr h_n_odd, ↓reduceIte]
+  obtain ⟨mult_gt0, h_mult_exception⟩ := h_n_exception
+  have n_gt0 : n > 0 := Odd.pos h_n_odd
+  have h_mult_mul_n_ge_n: u.multiplier * n ≥ n := by
+    have h_mult_ge_1 : u.multiplier ≥ 1 := Nat.one_le_iff_ne_zero.mpr mult_gt0
+    exact Nat.le_mul_of_pos_left n h_mult_ge_1
+  by_cases h : u.adder = 0
+  · have : u.multiplier > 1 := by
+      grind only
+    simp_all only [ne_eq, not_true_eq_false, or_false, gt_iff_lt, ge_iff_le, le_mul_iff_one_le_left,
+      add_zero, lt_mul_iff_one_lt_left]
+  · have : u.adder > 0 := by
+      exact Nat.pos_of_ne_zero h
+    exact lt_add_of_le_of_pos h_mult_mul_n_ge_n this
+
+
+
+-- If a universe is checkerboard, the universe is unbounded.
+-- This is because any odd number stays odd after applying the Collatz function.
+theorem checkerboardUnbounded (u : CollatzUniverse) (h : isCheckerboard u)
+  (h0_neq_m : u.multiplier ≠ 0) (h0_neq_a : u.adder ≠ 0) : isUnbounded u := by
+  -- We will again focus on 1 as the starting number.
+  -- We show that applying the Collatz function k times always results in an odd number.
+  have h_odd : ∀ k, Odd (applyCollatzKTimes u 1 k) := by
+    have := checkerboardImpliesAllOddToOddKTimes u h 1
+    simp_all only [odd_one, forall_const]
+  -- These universes are runaway,
+  -- since the odd numbers just keep getting multiplied by the multiplier.
+  have : isRunaway' u := by
+    unfold isRunaway'
+    use 1
+    intro k
+    set inner := applyCollatzKTimes u 1 k
+    rw [applyCollatzKTimes_add]
+    unfold applyCollatzKTimes
+    simp only [one_ne_zero, ↓reduceIte, tsub_self, applyCollatzKTimes_zero, gt_iff_lt]
+    have h_inner_odd : Odd inner := h_odd k
+    have h_odd_greater := oddStepGreater u ⟨ h0_neq_m, Decidable.not_or_of_imp fun a ↦ h0_neq_a ⟩
+      inner h_inner_odd
+    exact Nat.lt_of_succ_le h_odd_greater
+  rw [← runawayIffRunaway' u ] at this
+  exact runawayIffUnbounded u this
